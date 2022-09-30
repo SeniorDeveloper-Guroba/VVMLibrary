d11fd9b095d5f706843ae2d490c902403ca07f12
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-09-30 19:08:32 +0300

    refactor

--- Sources/GDVVMLibrary/ViewProtocol.swift
+++ Sources/GDVVMLibrary/ViewProtocol.swift
@@ -1,22 +1,4 @@
-//
-//  ViewProtocol.swift
-//  
-//
-//  Created by Senior Developer on 10.09.2022.
-//
-//import Foundation
-//
-//public protocol ViewProtocol where Self: AnyObject {
-//    
-//    associatedtype ViewProperties
-//    
-//    var viewProperties: ViewProperties? { get set }
-//    
-//    var updateViewProperties: Closure<ViewProperties?>? { get }
-//    var createViewProperties: Closure<ViewProperties?>? { get }
-//    
-//    func bind()
-//}
+
 import Foundation
 
 public protocol ViewProtocol where Self: AnyObject {
