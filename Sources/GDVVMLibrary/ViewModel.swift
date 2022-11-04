038403a5a8b592d2270e949b512a819e44a001f4
Aiub Duduev <aub.duduev.1989@gmail.com>
2022-11-04 15:45:55 +0300

    bag fixedrefactor

--- Sources/GDVVMLibrary/ViewModel.swift
+++ Sources/GDVVMLibrary/ViewModel.swift
@@ -6,11 +6,11 @@ open class ViewModel<View: ViewProtocol> {
     public var create: Closure<View.ViewProperties?>?
     
     // MARK: - Привязываем View с ViewModel
-    public static func bind(with view: View) -> ViewModel? {
+    public static func bind(with view: View) -> ViewModel {
         let viewModel = ViewModel<View>()
         viewModel.update = view.update(with:)
         viewModel.create = view.create(with:)
-        return viewModel as? Self
+        return viewModel as! Self
     }
     
     public init(){}
